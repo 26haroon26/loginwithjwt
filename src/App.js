f0d5a929273cb2b542dc110fa3e381548ecc1977
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-12-24 23:54:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import SignIn from './components/signin'
-import Login from './components/login'
-import Dashboard from './components/dashboard'
-import { BrowserRouter as Router,
-  Routes,
- Route,
- Link} from "react-router-dom";
+import "./App.css";
+import SignIn from "./components/signin";
+import Login from "./components/login";
+import Dashboard from "./components/dashboard";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+
+let baseUrl = "https://crazy-wrap-frog.cyclic.app";
 
 function App() {
+  const Logout = async () => {
+    try {
+      let response = await axios.post(`${baseUrl}/logout`);
+
+      console.log("logout successful");
+    } catch (e) {
+      console.log("e: ", e);
+    }
+  };
   return (
     <div className="App">
       <Router>
-        <div className='ForLinks'>
-      <Link to='/'>Sign In</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/dashboard'>Dashboard</Link>
+        <div className="ForLinks">
+          <Link to="/">Sign In</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <a onClick={Logout}>Logout</a>
         </div>
-     <Routes>
-      <Route index element={<SignIn/>}/>
-      <Route exact path='/login' element={<Login/>}/>
-      <Route exact path='/dashboard' element={<Dashboard/>}/>
-     </Routes>
-     
+        <Routes>
+          <Route index element={<SignIn />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+
       </Router>
     </div>
   );
